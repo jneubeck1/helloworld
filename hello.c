b80258d5fe0f36b0d030098cf78655a83c8cb5be
jneubeck1 <42659722+jneubeck1@users.noreply.github.com>
2018-08-23 18:35:01 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jeremy and Mohammed
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Jeremy and Mohammed\n");
+
+  return 0;
+}
