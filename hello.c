6ef95b10b21ec07cf973678e30970433d54c3f70
malfatle2 <42657476+malfatle2@users.noreply.github.com>
2018-08-23 20:00:47 -0500

    major

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hello Jeremy and Mohammed and Alec(ComputerScience)\n");
+  printf("Hello Jeremy and Mohammed software engineer and Alec(ComputerScience)\n");
 
   return 0;
 }
